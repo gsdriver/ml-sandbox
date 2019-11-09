33e465ecec6a4ff1ab104f09192686958850fb1e
Garrett Vargas <gsdriver@live.com>
2019-11-09 07:57:40 -0800

    Add cost function

--- slotmachine/ml.m
+++ slotmachine/ml.m
@@ -6,6 +6,15 @@ clear ; close all; clc
 
 fprintf('Loading Data ...\n')
 data = load('matrix.txt');
-X = data(:, 1:5); y = data(:, 6);
-m = length(y); % number of training examples
+[m, n] = size(data);
+X = data(:, 1:n-1);
+y = data(:, n);
 
+%  Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+initial_theta = zeros(n + 1, 1);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost
+[theta, cost] = ...
+    fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
