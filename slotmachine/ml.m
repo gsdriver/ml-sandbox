e583a83158b03f16cc4bb8745d98b61a8734300e
Garrett Vargas <garrettvargas@Garretts-MacBook-Pro.local>
2019-11-09 08:02:46 -0800

    Add x0

--- slotmachine/ml.m
+++ slotmachine/ml.m
@@ -9,10 +9,12 @@ data = load('matrix.txt');
 [m, n] = size(data);
 X = data(:, 1:n-1);
 y = data(:, n);
+n = n - 1;
 
 %  Set options for fminunc
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 initial_theta = zeros(n + 1, 1);
+X = [ones(m,1) X];
 
 %  Run fminunc to obtain the optimal theta
 %  This function will return theta and the cost
